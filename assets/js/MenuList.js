fa6e865299a631ce8ff2b565270cd363a286b2d7
Phạm Văn Diện <88573083+D-Jukie@users.noreply.github.com>
2022-06-19 21:00:54 +0700

    Add files via upload

--- assets/js/MenuList.js
+++ assets/js/MenuList.js
@@ -1,3 +1,4 @@
+endpoint = `https://api.phamvandien.xyz`
 message_list = [{
 	icon: 'fas fa-file-code',
 	title: 'Template Recode by m hadi f',
@@ -20,77 +21,16 @@ item_list = [{
 	name: 'Downloader',
 	icon: 'fas fa-arrow-down',
 	item: [{
-		"url": "/api/instagram?url=https://www.instagram.com/p/CMeFrnTp8as",
-		"name": "Instagram video foto downloader"
-	},
-		{
-			"url": "/api/ytvideo",
-			"name": "Yt video downloader"
-		},
-		{
-			"url": "/api/ytaudio",
-			"name": "Yt audio downloader"
-		},
-		{
-			"url": "/api/yt2/video",
-			"name": "Yt video downloader 2"
-		},
-		{
-			"url": "/api/yt2/audio",
-			"name": "Yt audio downloader 2"
-		},
-		{
-			"url": "/api/aboutyt",
-			"name": "Yt scraper"
-		},
-		{
-			"url": "/api/ytplay",
-			"name": "YtPlay"
-		},
-		{
-			"url": "/api/soundcloud/play",
-			"name": "sound cloud play"
-		},
-		{
-			"url": "/api/soundcloud/download",
-			"name": "sound cloud downloader"
-		},
-		{
-			"url": "/api/tiktok",
-			"name": "tiktok downloader"
-		},
-		{
-			event: 'NEW',
-			"url": "/api/pinterestv2?q=loli",
-			"name": "Pinterest V2"
-		},
-		{
-			"url": "/api/pinterest?q=loli",
-			"name": "Pinterest"
-		},
-		{
-			url: '/docs/top4top',
-			name: 'top4top uploader',
-		},
-		{
-			"url": "/api/googlesearch",
-			"name": "google result search"
-		},
-		{
-			"url": "/api/nhentai?id=386902",
-			"name": "nhentai pdf downloader"
-		},
-		{
-			"url": "/api/githubstalk?username=keqing",
-			"name": "Github stalk"
-		}]
+		"url": endpoint + "/tiktok?getData=https://vm.tiktok.com/ZMLMbkp82",
+		"name": "TikTok Downloader"
+	}]
 },
 	{
-		name: 'Contact',
+		name: 'Liên hệ Admin',
 		icon: 'fas fa-user-headset',
 		item: [{
 			event: 'DO NOT SPAM',
-			url: '0332222817',
-			name: 'Whatsapp'
+			url: 'https://www.facebook.com/PhamVanDien.User/',
+			name: 'Facebook'
 		}]
 	}];
