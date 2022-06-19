f1f3ada2366c35bef1c83df388317e311dcba46c
Phạm Văn Diện <88573083+D-Jukie@users.noreply.github.com>
2022-06-20 05:39:49 +0700

    Update MenuList.js

--- assets/js/MenuList.js
+++ assets/js/MenuList.js
@@ -18,11 +18,97 @@ message_list = [{
 }];
 
 item_list = [{
-	name: 'Downloader',
-	icon: 'fas fa-arrow-down',
+	name: 'TikTok',
+	icon: 'fa-brands fa-tiktok',
 	item: [{
 		"url": endpoint + "/tiktok?url=https://vm.tiktok.com/ZMLMbkp82",
 		"name": "TikTok Downloader"
+	},
+	{
+		"url": endpoint + "/tiktok?search=son tung",
+		"name": "TikTok Search"
+	},
+	{
+		"url": endpoint + "/tiktok?username=ltkh2004",
+		"name": "TikTok Info User"
+	}]
+},
+{
+	name: 'SoundCloud',
+	icon: 'fa-brands fa-soundcloud',
+	item: [{
+		"url": endpoint + '/soundcloud?search=son tung',
+		"name": "SoundCloud Search"
+	},
+	{
+		"url": endpoint + '/soundcloud?url=https://soundcloud.com/sontungmtp-music/ch-ng-ta-c-a-hi-n-t-i',
+		"name": "SoundCloud Downloader"
+	}]
+},
+{
+	name: 'Youtube',
+	icon: 'fa-brands fa-youtube',
+	item: [{
+		"url": endpoint + '/youtube?search=son tung',
+		"name": "Youtube Search"
+	},
+	{
+		"url": endpoint + '/youtube?GetVideoId=https://www.youtube.com/watch?v=Jk38OqdAQxc',
+		"name": "Get Video Id"
+	},
+	{
+		"url": endpoint + '/youtube?GetVideoDetails=xypzmu5mMPY',
+		"name": "Get Video Details"
+	},
+	{
+		"event": 'Some Error',
+		"url": endpoint + '/youtube?downloadVideo=Jk38OqdAQxc',
+		"name": "Youtube Downloader"
+	}]
+},
+{
+	name: 'Short Url',
+	icon: 'fa-solid fa-curling-stone',
+	item: [{
+		"url": endpoint + '/slink?url=https://www.youtube.com/watch?v=Jk38OqdAQxc',
+		"name": "Short Url"
+	}]
+},
+{
+	name: 'Pinterest',
+	icon: 'fa-brands fa-pinterest',
+	item: [{
+		"url": endpoint + '/pinterest?search=mirai',
+		"name": "Search"
+	}]
+},
+{
+	name: 'Random Images',
+	icon: 'fa-solid fa-images',
+	item: [{
+		"event": 'NSFW',
+		"url": endpoint + '/images/ausand',
+		"name": "Ausand"
+	},
+	{	
+		"event": 'NSFW',
+		"url": endpoint + '/images/du',
+		"name": "NSFW 1"
+	},
+	{
+		"event": 'NSFW',
+		"url": endpoint + '/images/girl',
+		"name": "NSFW 2"
+	},
+	{
+		"event": 'NSFW',
+		"url": endpoint + '/images/mong',
+		"name": "NSFW 3"
+	},
+	{
+		"event": 'NSFW',
+		"url": endpoint + '/images/anime',
+		"name": "Anime"
 	}]
 },
 	{
